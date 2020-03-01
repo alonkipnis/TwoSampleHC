0092f5078203e65c4f98530d2d0a4113208cff8e
alonkipnis@gmail.com <alonkipnis@gmail.com>
2020-03-01 14:31:14 -0800

    added test

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Two-sample Higher Criticism",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alonkipnis/TwoSampleHC",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
---
+++ tests/multinomial_perturbation.py
@@ -0,0 +1,21 @@
+N = 10000 # number of features
+n = 5 * N #number of samples
+
+P = 1 / np.arange(1,N+1) # Zipf base distribution
+P = P / P.sum()
+
+ep = 0.03 #fraction of features to perturb
+mu = 0.005 #intensity of perturbation
+
+TH = np.random.rand(N) < ep
+Q = P.copy()
+Q[TH] += mu
+Q = Q / np.sum(Q)
+
+smp_P = np.random.multinomial(n, P)  # sample form P
+smp_Q = np.random.multinomial(n, Q)  # sample from Q
+
+pv = two_sample_pvals(smp_Q, smp_P) # binomial P-values
+HC, p_th = hc_vals(pv, alpha = 0.25) # Higher Criticism test
+
+self.assertTrue(np.abs(HC - 21)<3)
