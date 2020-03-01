773c8bb8fc7390b7663dc4b68ace94f6104270ae
alonkipnis@gmail.com <alonkipnis@gmail.com>
2020-03-01 15:18:31 -0800

    ditribution version 0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TwoSampleHC-kipnisal", # Replace with your own username
+    name="TwoSampleHC", 
     version="0.0.1",
     author="Alon Kipnis",
     author_email="alonkipnis@gmail.com",
